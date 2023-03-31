f080877ddb0aadcbdd94a86d0b4b68d7e23a457b
Mervenur TOPCU <mervenur.tpcu@outlook.com>
2023-03-31 15:00:50 +0300

    changes are commited

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:desserts/features/dessert_view.dart';
 import 'package:desserts/product/initialize/application_start.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../product/constants/index.dart';
 
 Future<void> main() async {
   //Firebase proje başlangıç kodları
   await ApplicationStart.init();
-  runApp(const ProviderScope(child: MyApp()));
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
